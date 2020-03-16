c8196aa2ce7fee1691328941961016a1f0cf7b36
Shannon <scrowley016@gmail.com>
2020-03-16 14:13:11 -0400

    map and marker

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2Nyb3dsZXkwMTYiLCJhIjoiY2p1bmwzOXM0MnR0dzQzbzJjbzBpcTNteiJ9._moLkbeStYq-mXj3Pqrv7w';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
